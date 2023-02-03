0b89d1a53c68337f5a71f89d5d2926cf0f12dbd5
Aman kumar Patel <97835747+Amankumarpatel123@users.noreply.github.com>
2023-02-03 18:03:19 +0530

    Updates

---
+++ precache-manifest.511fd38491ef0bfd217e574da9e460fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44de2dceed1eb2a128bdcb317152a35b",
+    "url": "/Todo-List/index.html"
+  },
+  {
+    "revision": "33386f7ef8299d4b4c5e",
+    "url": "/Todo-List/static/css/main.0fd9da62.chunk.css"
+  },
+  {
+    "revision": "f0a6578a2d412803e094",
+    "url": "/Todo-List/static/js/2.f117899b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Todo-List/static/js/2.f117899b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33386f7ef8299d4b4c5e",
+    "url": "/Todo-List/static/js/main.48da1e49.chunk.js"
+  },
+  {
+    "revision": "0794853386fe83b57ff1",
+    "url": "/Todo-List/static/js/runtime-main.dbe8b203.js"
+  }
+]);
